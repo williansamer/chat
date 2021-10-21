ba8472054dd8694bb979fab2b79f8637038d5035
williansamer <willian.samer@gmail.com>
2021-10-21 10:57:41 -0300

    criando salas para o chat

--- index.js
+++ index.js
@@ -3,23 +3,46 @@ const app = express();
 const path = require("path");
 const socketIO = require("socket.io");
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/lazer", express.static(path.join(__dirname, "public")));
+app.use("/trabalho", express.static(path.join(__dirname, "public")));
 
 const server = app.listen(3000, ()=>{
     console.log("Server running..");
 })
 
-const messages = [];
+const messages = {lazer: [], trabalho: []};
 
 const io = socketIO(server);
 
-io.on("connection", (socket)=>{ //
+const lazer = io.of("/lazer").on("connection", (socket)=>{ // O 'of' permite que crie uma nova 'sala'. Esta sala vai ser chamada lá pelo FRONT: . OBS: este '/lazer' não tem ligação DIRETA com a linha 6. Tem ligação com a chamativa do FRONT em: 'const socket = io(`http://localhost:3000${room}`);'.
+    console.log("New connection");
+
+    socket.emit("messages", messages.lazer);
+
+    socket.on("new-message", (data)=>{
+        messages.lazer.push(data);
+        lazer.emit("messages", messages.lazer) //Agora é o 'lazer' que irá emitir para TODOS os usuários o array - 'messages.lazer'
+    })
+})
+
+const trabalho = io.of("/trabalho").on("connection", (socket)=>{ // O 'of' permite que crie uma nova 'sala'. Esta sala vai ser chamada lá pelo FRONT: 
+    console.log("New connection");
+
+    socket.emit("messages", messages.trabalho);
+
+    socket.on("new-message", (data)=>{
+        messages.trabalho.push(data);
+        trabalho.emit("messages", messages.trabalho) //Agora é o 'trabalho' que irá emitir para TODOS os usuários o array - 'messages.trabalho'
+    })
+})
+
+/* io.on("connection", (socket)=>{ //
     console.log("New connection");
 
     socket.emit("messages", messages); //enviado para TODOS os FRONTs ao entrar no chat
 
     socket.on("new-message", (data)=>{
-        messages.push(data); //mandando a nova mensagem para o array de mensagens(messages)
+        messages.push(data); //mandando a nova mensagem para o array de mensagens(messages). Como colocou somente 'data', ele manda tudo que tem nele, no caso, o user e o msg
         io.emit("messages", messages) //enviando para TODOS OS FRONTs, o array de mensagens
     })
-})
+}) */
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,6 @@
-const socket = io("http://localhost:3000"); //conectando com o BACK
+const room  = window.location.pathname.replace(/\//g, ''); // O  'replace(/\//g, '')' irá substituir TUDO que for /(barra) por 'vazio'. A leitura do regex é: O Barra(/) irá mostrar o começo e o final de uma string. O '\/' irá informar a string que deseja(no caso, usou a barra contrária para dar certo, senão ficaria como comentário), é por fim o 'g' para usar de forma global
+//console.log(room);
+const socket = io(`http://localhost:3000/${room}`); //conectando com o BACK(com as respectivas salas)
 
 socket.on("messages", (messages)=>{ //Recebendo do BACK, o array de mensagens(messages)
     insertMessagesOnMural(messages); //enviando como arg o 'messages'
@@ -10,7 +12,7 @@ function insertMessagesOnMural(messages){
     let list_messages = "<ul>";
 
     messages.forEach((message)=>{
-        list_messages += `<li>${message.user}_____: ${message.msg}</li>`;
+        list_messages += `<li>${message.user}_____: ${message.msg}</li>`; //Definindo no li o user e o msg
     })
     list_messages +="</ul>";
 
@@ -21,14 +23,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const form = document.querySelector("#fmn"); //criando uma const do formulário(form)
     form.addEventListener("submit", function(e){ //criando um evento(addEventListener) dentro do form. O 'e' é referente ao evento
         e.preventDefault(); //não deixa(evita) o evento(e) usar o seu comportamento padrão. Desviando, assim, o foco do comportamento do evento para outra direção
-        if(!user){
-            alert("Defina o nome para continuar");
-            return;
+        if(!user){ //Se o 'user' for vazio(null)..
+            alert("Defina o nome para continuar"); //alert
+            return; //Para a leitura
         }
         let inpMessage = document.forms["form-message-name"]["input-message"].value;//Pegando o valor(value) do input. OBS. ["form-message-name"] e ["input-message"], é referente ao 'name' colocado lá no index.html. É uma outra forma de pegar os dados do input, ao invés de usar o 'querySelector'
         document.forms["form-message-name"]["input-message"].value = '';
         if(!inpMessage == ''){//Se o campo do input não for vazio, faça..
-            socket.emit("new-message", {user: user, msg: inpMessage}); //Enviango para o BACK, a msg do input pelo caminho do 'new-message'
+            socket.emit("new-message", {user: user, msg: inpMessage}); //Enviando para o BACK, o user e a msg do input pelo caminho do 'new-message'
         }
     })
 
@@ -36,6 +38,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
     userForm.addEventListener("submit", function(e){
         e.preventDefault();
         user = document.forms["user-message-name"]["input-user"].value;
-        userForm.parentNode.removeChild(userForm);
+        userForm.parentNode.removeChild(userForm); //Removendo o form. Pega o 'pai' do form como referência e exclui o filho, ou seja, ele próprio.
     })
 })
