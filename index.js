9982288b9df9d21a5bdcc7f8673f3e3e79a6b81c
williansamer <willian.samer@gmail.com>
2021-10-22 09:47:44 -0300

    Criado a pasta views p/ os arquivos EJS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const socketIO = require("socket.io");
 app.use("/lazer", express.static(path.join(__dirname, "public")));
 app.use("/trabalho", express.static(path.join(__dirname, "public")));
 
-app.set("views", path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res)=>{
